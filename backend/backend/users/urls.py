08bc072ab4d8e3bf7a3f30ce369e5252acbc0ee2
jayanth142006 <166970063+jayanth142006@users.noreply.github.com>
2025-07-09 22:36:04 +0530

    Delete backend/backend directory

--- backend/backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from users.views import LoginView, RegisterView
-
-
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('signup/', RegisterView.as_view(), name='signup'),
-]
